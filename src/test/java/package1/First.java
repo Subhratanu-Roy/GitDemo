8505ec1671e25c21914e19547654828b9ce10e42
subhratanu <subhratanu15@gmail.com>
2021-12-19 20:27:34 +0530

    Class 'First' is added with two display methods

---
+++ src/test/java/package1/First.java
@@ -0,0 +1,19 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class First {
+
+	
+	@Test
+	public void display()
+	{
+		System.out.println("This is first class");
+	}
+	
+	@Test
+	public void display2()
+	{
+		System.out.println("This is first class2");
+	}
+}
